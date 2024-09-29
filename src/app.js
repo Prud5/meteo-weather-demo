b560929f35604d39762fabca56e54c5daf6eb485
Prud5 <prudencelynne56@gmail.com>
2024-09-29 18:17:32 +0300

    Updated index.html

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ function refreshWeather(response) {
   }
   
   function searchCity(city) {
-    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiKey = "c33f4afaf060ba22bf444fe3do4a19ft";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(refreshWeather);
   }
@@ -62,7 +62,7 @@ function refreshWeather(response) {
   }
   
   function getForecast(city) {
-    let apiKey = "b2a5adcct04b33178913oc335f405433";
+    let apiKey = "c33f4afaf060ba22bf444fe3do4a19ft";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
     axios(apiUrl).then(displayForecast);
   }
